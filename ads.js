e43606fcb5d0d7d8cd0c8e92fd67159ac5b62089
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-01-23 20:47:11 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,38 @@
+const testGroups = {
+    "Advertising": [
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+        "https://adservice.google.com/adsid/integrator.js",
+        "https://z-na.amazon-adsystem.com/widgets/onejs",
+        "https://static.ads-twitter.com/uwt.js",
+        "https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+    ],
+    "Analytics & Tracking": [
+        "https://www.google-analytics.com/analytics.js",
+        "https://static.hotjar.com/c/hotjar.js",
+        "https://cdn.mouseflow.com/projects/test.js",
+        "https://mc.yandex.ru/metrika/tag.js"
+    ],
+    "Social Pixels": [
+        "https://connect.facebook.net/en_US/fbevents.js",
+        "https://analytics.tiktok.com/i18n/pixel/sdk.js",
+        "https://sc-static.net/scevent.min.js",
+        "https://www.redditstatic.com/ads/pixel.js"
+    ],
+    "Pop-ups & Annoyances": [
+        "https://onclickads.net/a/display.php",
+        "https://yllix.com/wp-content/themes/yllix/assets/js/scripts.js",
+        "https://popads.net/main/app.js",
+        "https://engine.adzerk.net/ados.js"
+    ],
+    "Crypto Miners": [
+        "https://coin-hive.com/lib/coinhive.min.js",
+        "https://authedmine.com/lib/authedmine.min.js",
+        "https://crypto-loot.com/lib/miner.js"
+    ],
+    "Adult Ads": [
+        "https://as.sexad.net/js/abc.js",
+        "https://creative.trafficjunky.com/p/top.js",
+        "https://pt.trafficstars.com/api/v1/ad-tag.js",
+        "https://cdn.exoclick.com/ads.js"
+    ]
+};
